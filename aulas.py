cee6b6b328ea1d17936e9d8ed6b1a2a248d85348
nataliaoliveiradev <profissionalnataliaoliveira@gmail.com>
2024-08-01 19:55:22 -0300

    exercícios com variáveis

---
+++ aulas.py
@@ -0,0 +1,12 @@
+#função print, usamos aspas simples ou duplas para strings
+print('Olá, Mundo!')
+#podemos inserir operações aritméticas dentro do parêntese, nesse caso, não se usa aspas
+print(2*2*2*2*2)
+print(10*(5+7)/4)
+#podemos usar o + ou , para fazer concatenação de duas strings, não faz diferença
+print('2'+'2')
+print('squirtle','charmander','bulbassaur')
+# Escreva as seguintes expressões matemáticas em linguagem Python: a. 2 + 3 x 3 / b. 4² ÷ 3 / c. (9² + 2) x 6 - 1
+print(2+(3*3))
+print((4**2)/3)
+print(((9**2+2)*6)-1)
---
+++ variaveis.py
@@ -0,0 +1,5 @@
+##No código a seguir, estamos criando duas variáveis (não vamos tratar do tipo, ainda). A primeira tem nome disciplina. A outra se chama nota e armazena uma nota tirada por alguém que cursa a disciplina
+disciplina = 'Lógica de Programação e Algoritmos'
+nota = 8.5
+print(disciplina)
+print(nota)
