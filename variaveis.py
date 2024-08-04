f42c35a3ada6c054d32665d69c1372c5e146aeaf
nataliaoliveiradev <profissionalnataliaoliveira@gmail.com>
2024-08-03 21:16:50 -0300

    exercícios com dados booleanos

--- variaveis.py
+++ variaveis.py
@@ -3,3 +3,13 @@ disciplina = 'Lógica de Programação e Algoritmos'
 nota = 8.5
 print(disciplina)
 print(nota)
+##Douglas Adams nos ensinou que a resposta para o sentido da vida, do  Universo e tudo mais é 42. Armazene, então, em uma variável, o sentido da vida. Crie uma mensagem que imprima na tela essa variável, junto de uma frase dizendo que ela é o sentido da vida.
+texto = 'O sentido da vida é'
+sentido = 42
+print(texto,sentido)
+# Vejamos o exemplo em que a variável a contém o valor 1 e a variável b, o valor 5.
+a = 1 #a recebe 1
+b = 5 #b recebe 5
+#resposta recebe o resultado da comparação lógica de igualdade
+resposta = a == b 
+print('A resposta é:', resposta)
